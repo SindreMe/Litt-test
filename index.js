cfab39bef4730b25fdea28a155ce9575bd1dc548
SindreMe <122259979+SindreMe@users.noreply.github.com>
2023-02-10 09:44:42 +0100

    10.02

--- index.js
+++ index.js
@@ -110,76 +110,76 @@ goToA();
 
 //----------------------------------------------------------------------------
 
-goToE1();
+goToEA();
 
-function goToE1() {
+function goToEA() {
     document.getElementById('game2').innerHTML = /*html*/`
     You are in room E. There are 4 doors:
     <ul>
-        <button onclick="goToB1()">Go to B</button>
-        <button onclick="goToD1()">Go to D</button>
-        <button onclick="goToF1()">Go to F</button>
-        <button onclick="goToG1()">Go to G</button>
+        <button onclick="goToBA()">Go to B</button>
+        <button onclick="goToDA()">Go to D</button>
+        <button onclick="goToFA()">Go to F</button>
+        <button onclick="goToGA()">Go to G</button>
     </ul>
      `;
 }
 
-function goToF1() {
+function goToFA() {
     document.getElementById('game2').innerHTML = /*html*/`
     You are in room F. There are 2 doors:
     <ul>
-        <button onclick="goToE1()">Go to E</button>
-        <button onclick="goToC1()">Go to C</button>
+        <button onclick="goToEA()">Go to E</button>
+        <button onclick="goToCA()">Go to C</button>
     </ul>
     `;
 }
 
-function goToG1() {
+function goToGA() {
     document.getElementById('game2').innerHTML = /*html*/`
     You are in room G. You have won! There are 1 doors:
     <ul>
-        <button onclick="goToE1()">Go to E</button>
+        <button onclick="goToEA()">Go to E</button>
     </ul>
     `;
 }
 
-function goToB1() {
+function goToBA() {
     document.getElementById('game2').innerHTML = /*html*/`
     You are in room B. There are 3 doors:
     <ul>
-        <button onclick="goToA1()">Go to A</button>
-        <button onclick="goToC1()">Go to C</button>
-        <button onclick="goToE1()">Go to E</button>
+        <button onclick="goToAA()">Go to A</button>
+        <button onclick="goToCA()">Go to C</button>
+        <button onclick="goToEA()">Go to E</button>
     </ul>
     `;
 }
 
-function goToA1() {
+function goToAA() {
     document.getElementById('game2').innerHTML = /*html*/`
     You are in room A. There are 2 doors:
     <ul>
-        <button onclick="goToB1()">Go to B</button>
-        <button onclick="goToD1()">Go to D</button>
+        <button onclick="goToBA()">Go to B</button>
+        <button onclick="goToDA()">Go to D</button>
     </ul>
     `;
 }
 
-function goToD1() {
+function goToDA() {
     document.getElementById('game2').innerHTML = /*html*/`
     You are in room D. There are 2 doors:
     <ul>
-        <button onclick="goToA1()">Go to A</button>
-        <button onclick="goToE1()">Go to E</button>
+        <button onclick="goToAA()">Go to A</button>
+        <button onclick="goToEA()">Go to E</button>
     </ul>
     `;
 }
 
-function goToC1() {
+function goToCA() {
     document.getElementById('game2').innerHTML = /*html*/`
     You are in room C. There are 3 doors:
     <ul>
-        <button onclick="goToB1()">Go to B</button>
-        <button onclick="goToF1()">Go to F</button>
+        <button onclick="goToBA()">Go to B</button>
+        <button onclick="goToFA()">Go to F</button>
     </ul>
     `;
 }
@@ -196,6 +196,285 @@ function pauseAudio() {
   x.pause();
 } 
 
+//----------------------------------------------------------------------------
 
+walkToA();
 
+function walkToA() {
+    document.getElementById('game3').innerHTML = /*html*/`
+    <h2>You are in a forest, find your way out:</h2>
+    <p>
+        <button onclick="walkToF()">Go down</button>
+        <button onclick="walkToB()">Go right</button>
+    </p>
+     `;
+}
 
+function walkToB() {
+    document.getElementById('game3').innerHTML = /*html*/`
+    <h2>Rom B:</h2>
+    <p>
+        <button onclick="walkToA()">Go left</button>
+        <button onclick="walkToG()">Go down</button>
+        <button onclick="walkToC()">Go right</button>
+    </p>
+     `;
+}
+
+function walkToC() {
+    document.getElementById('game3').innerHTML = /*html*/`
+    <h2>Rom C:</h2>
+    <p>
+        <button onclick="walkToB()">Go left</button>
+        <button onclick="walkToH()">Go down</button>
+    </p>
+     `;
+}
+
+function walkToG() {
+    document.getElementById('game3').innerHTML = /*html*/`
+    <h2>Rom G:</h2>
+    <p>
+        <button onclick="walkToF()">Go left</button>
+        <button onclick="walkToL()">Go down</button>
+        <button onclick="walkToB()">Go up</button>
+        <button onclick="walkToH()">Go right</button>
+    </p>
+     `;
+}
+
+function walkToF() {
+    document.getElementById('game3').innerHTML = /*html*/`
+    <h2>Rom F:</h2>
+    <p>
+        <button onclick="walkToA()">Go up</button>
+        <button onclick="walkToK()">Go down</button>
+        <button onclick="walkToG()">Go right</button>
+    </p>
+     `;
+}
+
+function walkToK() {
+    document.getElementById('game3').innerHTML = /*html*/`
+    <h2>Rom K:</h2>
+    <p>
+        <button onclick="walkToF()">Go up</button>
+        <button onclick="walkToP()">Go down</button>
+        <button onclick="walkToL()">Go right</button>
+    </p>
+     `;
+}
+
+function walkToP() {
+    document.getElementById('game3').innerHTML = /*html*/`
+    <h2>Rom P:</h2>
+    <p>
+        <button onclick="walkToK()">Go up</button>
+    </p>
+     `;
+}
+
+function walkToL() {
+    document.getElementById('game3').innerHTML = /*html*/`
+    <h2>Rom L:</h2>
+    <p>
+        <button onclick="walkToF()">Go left</button>
+        <button onclick="walkToQ()">Go down</button>
+        <button onclick="walkToG()">Go up</button>
+        <button onclick="walkToM()">Go right</button>
+    </p>
+     `;
+}
+
+function walkToQ() {
+    document.getElementById('game3').innerHTML = /*html*/`
+    <h2>Rom Q:</h2>
+    <p>
+        <button onclick="walkToP()">Go left</button>
+        <button onclick="walkToV()">Go down</button>
+        <button onclick="walkToL()">Go up</button>
+        <button onclick="walkToR()">Go right</button>
+    </p>
+     `;
+}
+
+function walkToV() {
+    document.getElementById('game3').innerHTML = /*html*/`
+    <h2>Rom V:</h2>
+    <p>
+        <button onclick="walkToU()">Go left</button>
+        <button onclick="walkToQ()">Go up</button>
+        <button onclick="walkToW()">Go right</button>
+    </p>
+     `;
+}
+
+function walkToU() {
+    document.getElementById('game3').innerHTML = /*html*/`
+    <h2>Rom U:</h2>
+    <p>
+        <button onclick="walkToV()">Go right</button>
+    </p>
+     `;
+}
+
+function walkToW() {
+    document.getElementById('game3').innerHTML = /*html*/`
+    <h2>Rom W:</h2>
+    <p>
+        <button onclick="walkToV()">Go left</button>
+        <button onclick="walkToR()">Go up</button>
+    </p>
+     `;
+}
+
+function walkToR() {
+    document.getElementById('game3').innerHTML = /*html*/`
+    <h2>Rom R:</h2>
+    <p>
+        <button onclick="walkToQ()">Go left</button>
+        <button onclick="walkToW()">Go down</button>
+        <button onclick="walkToM()">Go up</button>
+        <button onclick="walkToS()">Go right</button>
+    </p>
+     `;
+}
+
+function walkToM() {
+    document.getElementById('game3').innerHTML = /*html*/`
+    <h2>Rom M:</h2>
+    <p>
+        <button onclick="walkToL()">Go left</button>
+        <button onclick="walkToR()">Go down</button>
+        <button onclick="walkToH()">Go up</button>
+    </p>
+     `;
+}
+
+function walkToH() {
+    document.getElementById('game3').innerHTML = /*html*/`
+    <h2>Rom H:</h2>
+    <p>
+        <button onclick="walkToG()">Go left</button>
+        <button onclick="walkToM()">Go down</button>
+        <button onclick="walkToC()">Go up</button>
+        <button onclick="walkToI()">Go right</button>
+    </p>
+     `;
+}
+
+function walkToI() {
+    document.getElementById('game3').innerHTML = /*html*/`
+    <h2>Rom I:</h2>
+    <p>
+        <button onclick="walkToH()">Go left</button>
+        <button onclick="walkToN()">Go down</button>
+        <button onclick="walkToD()">Go up</button>
+    </p>
+     `;
+}
+
+function walkToD() {
+    document.getElementById('game3').innerHTML = /*html*/`
+    <h2>Rom D:</h2>
+    <p>
+        <button onclick="walkToC()">Go left</button>
+        <button onclick="walkToI()">Go down</button>
+        <button onclick="walkToE()">Go right</button>
+    </p>
+     `;
+}
+
+function walkToE() {
+    document.getElementById('game3').innerHTML = /*html*/`
+    <h2>Rom E:</h2>
+    <p>
+        <button onclick="walkToD()">Go left</button>
+        <button onclick="walkToJ()">Go down</button>
+    </p>
+     `;
+}
+
+function walkToJ() {
+    document.getElementById('game3').innerHTML = /*html*/`
+    <h2>Rom J:</h2>
+    <p>
+        <button onclick="walkToI()">Go left</button>
+        <button onclick="walkToE()">Go up</button>
+        <button onclick="walkToO()">Go down</button>
+    </p>
+     `;
+}
+
+function walkToO() {
+    document.getElementById('game3').innerHTML = /*html*/`
+    <h2>Rom O:</h2>
+    <p>
+        <button onclick="walkToN()">Go left</button>
+        <button onclick="walkToJ()">Go up</button>
+        <button onclick="walkToT()">Go down</button>
+    </p>
+     `;
+}
+
+function walkToN() {
+    document.getElementById('game3').innerHTML = /*html*/`
+    <h2>Rom N:</h2>
+    <p>
+        <button onclick="walkToM()">Go left</button>
+        <button onclick="walkToI()">Go up</button>
+        <button onclick="walkToS()">Go down</button>
+    </p>
+     `;
+}
+
+function walkToS() {
+    document.getElementById('game3').innerHTML = /*html*/`
+    <h2>Rom S:</h2>
+    <p>
+        <button onclick="walkToR()">Go left</button>
+        <button onclick="walkToN()">Go up</button>
+        <button onclick="walkToX()">Go down</button>
+    </p>
+     `;
+}
+
+function walkToX() {
+    document.getElementById('game3').innerHTML = /*html*/`
+    <h2>Rom X:</h2>
+    <p>
+        <button onclick="walkToW()">Go left</button>
+        <button onclick="walkToS()">Go up</button>
+    </p>
+     `;
+}
+
+function walkToT() {
+    document.getElementById('game3').innerHTML = /*html*/`
+    <h2>Rom T:</h2>
+    <p>
+        <button onclick="walkToO()">Go up</button>
+        <button onclick="walkToY()">Go down</button>
+    </p>
+     `;
+}
+
+function walkToY() {
+    document.getElementById('game3').innerHTML = /*html*/`
+    <h2>Rom Y:</h2>
+    <p>
+        <button onclick="walkToX()">Go left</button>
+        <button onclick="walkToT()">Go up</button>
+        <button onclick="walkToZ()">Go right</button>
+    </p>
+     `;
+}
+
+function walkToZ() {
+    document.getElementById('game3').innerHTML = /*html*/`
+    <h2>You made it out!</h2>
+    <p>
+
+    </p>
+     `;
+}
